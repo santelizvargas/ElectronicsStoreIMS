2fce1009fc73ec41660fbe5a8c5625033cef0c29
Brandon Santeliz <79380665+santelizvargas@users.noreply.github.com>
2024-07-17 22:10:47 -0600

    IMS-16: Create Multiplatform APP (SwiftUI) (#7)
    ### Related ticket
    
    Ticket: [IMS-16: Create Multiplatform APP
    (SwiftUI)](https://developer-solutions.atlassian.net/browse/IMS-XXX)
    
    ### Description
    
    The multiplatform app was created and tested
    
    ### Medias (if needed)
    
    - NONE
    
    
    ### Testplan
    
    - [x] I tested this change on my local environment.
    - [x] I ran the project using multiple simulators in Xcode.
    - [x] I ran the project using Real device (if applicable).
    - [ ] I ran the unit tests.
    - [ ] I requested this endpoint on Postman (if applicable).
    - [ ] I requested this endpoint on Swagger documentation (if
    applicable).
    
    ### Checklist
    
    - [ ] I added tests for this change.
    - [x] I checked the code style and run the linter.
    - [ ] I added necessary documentation (if applicable).

---
+++ ims-apple/IMS Apple/IMS_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMS_AppleApp.swift
+//  IMS Apple
+//
+//  Created by Steven Santeliz on 17/7/24.
+//
+
+import SwiftUI
+
+@main
+struct IMS_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
