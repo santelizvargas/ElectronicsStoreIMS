d5e36285c3a49e7d1d1124e761bc575525d30bfe
santelizvargas <ushija6@gmail.com>
2024-07-17 22:02:31 -0600

    feat: Add multiplatform app

---
+++ ims-apple/IMS Apple/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  IMS Apple
+//
+//  Created by Steven Santeliz on 17/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Multiplatform App")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
