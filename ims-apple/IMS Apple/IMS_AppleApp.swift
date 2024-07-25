194dc7a59e95516691ae938d0d95ab1c2cda7bee
santelizvargas <ushija6@gmail.com>
2024-07-24 20:19:19 -0600

    feat: Create SwiftData Manager
    - Added the DataManager class.
    - Created the SwiftDataProvider as Singleton class.

--- ims-apple/IMS Apple/IMS_AppleApp.swift
+++ ims-apple/IMS Apple/IMS_AppleApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct IMS_AppleApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(SwiftDataProvider.shared.container)
         }
     }
 }
