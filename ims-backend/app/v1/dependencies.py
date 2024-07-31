38e2cc8e5ded841897ec0563456e37be2d0d241e
santelizvargas <ushija6@gmail.com>
2024-07-30 20:25:29 -0600

    refactor(backed): Removed FastAPI references

--- ims-backend/app/v1/dependencies.py
+++
@@ -1,9 +0,0 @@
-"""
-    Dependencies which can be used for DI
-"""
-from functools import lru_cache
-from v1.core.models.database_settings import DatabaseSettings
-
-@lru_cache()
-def create_database_settings() -> DatabaseSettings:
-    return DatabaseSettings()
