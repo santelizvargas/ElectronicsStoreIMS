02807aae0d9e110f97501e32aaf08f21f4b279be
DerianCordobaPerez <derianricardo451@gmail.com>
2024-08-21 19:20:06 -0600

    feat: Added the role field in user response

--- ims-backend/src/apps/ims/api/infrastructure/authentication/repositories/PrismaAuthenticationRepository.ts
+++ ims-backend/src/apps/ims/api/infrastructure/authentication/repositories/PrismaAuthenticationRepository.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
 import { AuthenticationRepository } from '../../../domain/models/authentication/repositories/AuthenticationRepository';
 import { User } from '../../../domain/models/authentication/User';
 import { OmittedUser } from '../../../application/authentication/RegistrationService';
@@ -6,26 +6,60 @@ import { OmittedUser } from '../../../application/authentication/RegistrationSer
 export default class PrismaAuthenticationRepository implements AuthenticationRepository {
   constructor(private database: PrismaClient) {}
 
-  public async create(data: OmittedUser): Promise<User> {
-    return await this.database.user.create({
+  public async create(data: OmittedUser): Promise<User & { roles: Role[] }> {
+    const user = await this.database.user.create({
+      relationLoadStrategy: 'join',
+      include: {
+        roles: {
+          select: {
+            role: true,
+          },
+        },
+      },
       data: {
         ...data,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
     });
+
+    return {
+      ...user,
+      roles: user.roles.map(({ role }) => role),
+    };
   }
 
   public async findAll(): Promise<Omit<User, 'password'>[]> {
-    return await this.database.user.findMany({
+    const users = await this.database.user.findMany({
+      relationLoadStrategy: 'join',
+      include: {
+        roles: {
+          select: {
+            role: true,
+          },
+        },
+      },
       omit: {
         password: true,
       },
     });
+
+    return users.map((user) => ({
+      ...user,
+      roles: user.roles.map(({ role }) => role),
+    }));
   }
 
   public async findByCredentials(email: string, password: string): Promise<User | null> {
     return await this.database.user.findFirst({
+      relationLoadStrategy: 'join',
+      include: {
+        roles: {
+          select: {
+            role: true,
+          },
+        },
+      },
       where: {
         email,
         password,
@@ -35,6 +69,14 @@ export default class PrismaAuthenticationRepository implements AuthenticationRep
 
   public async updatePassword(email: string, password: string): Promise<User> {
     return await this.database.user.update({
+      relationLoadStrategy: 'join',
+      include: {
+        roles: {
+          select: {
+            role: true,
+          },
+        },
+      },
       where: {
         email,
       },
