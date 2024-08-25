7bb275a88eaa630a9e3e7f6c955cb8764b6b62db
DerianCordobaPerez <derianricardo451@gmail.com>
2024-08-24 23:12:59 -0600

    wip

--- ims-backend/src/apps/ims/api/infrastructure/express/controllers/invoices/CreateInvoiceController.ts
+++ ims-backend/src/apps/ims/api/infrastructure/express/controllers/invoices/CreateInvoiceController.ts
@@ -5,6 +5,8 @@ import { Controller } from '../../../../../shared/infrastructure/express/control
 import { RequestValidator } from '../../../../../shared/infrastructure/express/RequestValidator';
 import { ErrorHandler } from '../../../../../shared/domain/ErrorHandler';
 import CreateInvoiceService, { CreateInvoiceResponse } from '../../../../application/invoices/CreateInvoiceService';
+import { CrudRepository } from '../../../../../shared/domain/models/repository/CrudRepository';
+import { Product } from '@prisma/client';
 
 export default class CreateInvoiceController implements Controller {
   public readonly rules = [
@@ -14,11 +16,37 @@ export default class CreateInvoiceController implements Controller {
     RequestValidator,
   ];
 
-  constructor(private readonly createInvoiceService: CreateInvoiceService) {}
+  constructor(
+    private readonly createInvoiceService: CreateInvoiceService,
+    private readonly productRepository: CrudRepository<{ id: number; stock: number }, Product>,
+  ) {}
 
   async invoke(request: Request, response: Response, next: NextFunction): Promise<Response | void> {
     try {
+      // We need to update the product stock
+      request.body.products.forEach(async (product: { id: number; quantity: number }) => {
+        const foundProduct = await this.productRepository.find(product.id);
+        const productStock = foundProduct?.stock || 0;
+
+        if (productStock < product.quantity) {
+          return next(
+            response.json({
+              message: `Not enough stock for the product ${foundProduct?.name || product.id}`,
+              code: status.BAD_REQUEST,
+            }),
+          );
+        }
+
+        const newStock = productStock - product.quantity;
+
+        this.productRepository.update({
+          id: product.id,
+          stock: newStock,
+        });
+      });
+
       const invoiceResponse: CreateInvoiceResponse = await this.createInvoiceService.invoke(request.body);
+
       return response.json(invoiceResponse).status(status.CREATED);
     } catch (error) {
       const { message } = error as Error;
