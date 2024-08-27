1ccce73df593ee0f55ae17d67de3e4b20c938fb0
Brandon Santeliz <79380665+santelizvargas@users.noreply.github.com>
2024-08-27 01:21:11 -0600

    Access control (#83)
    ### Related ticket
    
    Ticket: [PR
    Title](https://developer-solutions.atlassian.net/browse/IMS-XXX)
    
    ### Description
    
    (Provide context and explain why you are making the change you are
    making, and why it is the right one)
    
    ### Medias (if needed)
    
    (Provide images or videos about the changes)
    
    ### Testplan
    
    - [ ] I tested this change on my local environment.
    - [ ] I ran the project using multiple simulators in Xcode.
    - [ ] I ran the project using Real device (if applicable).
    - [ ] I ran the unit tests.
    - [ ] I requested this endpoint on Postman (if applicable).
    - [ ] I requested this endpoint on Swagger documentation (if
    applicable).
    
    ### Checklist
    
    - [ ] I added tests for this change.
    - [ ] I checked the code style and run the linter.
    - [ ] I added necessary documentation (if applicable).

--- ims-apple/IMS Apple/Views/User/ProfileModels.swift
+++ ims-apple/IMS Apple/Views/User/ProfileModels.swift
@@ -11,11 +11,16 @@ struct UserInformation {
     let names: String
     let lastName: String
     let email: String
+    let role: UserRole
     
-    init(names: String, lastName: String, email: String) {
+    init(names: String,
+         lastName: String,
+         email: String,
+         role: UserRole) {
         self.names = names
         self.lastName = lastName
         self.email = email
+        self.role = role
     }
     
     /// Factory from user model
@@ -23,6 +28,7 @@ struct UserInformation {
         self.names = user?.firstName ?? ""
         self.lastName = user?.lastName ?? ""
         self.email = user?.email ?? ""
+        self.role = UserRole(rawValue: user?.roleId ?? 1) ?? .seller
     }
 }
 
