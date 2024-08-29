51521161a5180ee11f056646e5efbf17c3906cfa
santelizvargas <ushija6@gmail.com>
2024-08-28 19:58:10 -0600

    feat: Add product export

--- ims-apple/IMS Apple/Utilities/Exporter/ExporterButton.swift
+++ ims-apple/IMS Apple/Utilities/Exporter/ExporterButton.swift
@@ -7,6 +7,39 @@
 
 import SwiftUI
 
+struct ExporterNav<T: Collection>: View {
+    @State private var isPresented: Bool = false
+
+    private let fileName: String
+    private let collection: T
+    
+    init(fileName: String, collection: T) {
+        self.fileName = fileName
+        self.collection = collection
+    }
+    
+    var body: some View {
+        Button {
+            isPresented.toggle()
+        } label: {
+            Image(systemName: "square.and.arrow.up.fill")
+        }
+        .buttonStyle(.plain)
+        .fileExporter<IMSDocument>(
+            isPresented: $isPresented,
+            document: formattedDocument(collection: collection),
+            contentType: .commaSeparatedText,
+            defaultFilename: fileName) { result in
+                switch result {
+                    case .success(let success):
+                        debugPrint("Saved on: \(success.description)")
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                }
+            }
+    }
+}
+
 struct ExporterButton<IMSCollection: Collection>: View {
     @State private var isPresented: Bool = false
 
@@ -31,7 +64,7 @@ struct ExporterButton<IMSCollection: Collection>: View {
         )
         .fileExporter<IMSDocument>(
             isPresented: $isPresented,
-            document: formattedDocument,
+            document: formattedDocument(collection: collection),
             contentType: .commaSeparatedText,
             defaultFilename: fileName) { result in
                 switch result {
@@ -42,16 +75,18 @@ struct ExporterButton<IMSCollection: Collection>: View {
                 }
             }
     }
-    
-    // MARK: - Formatted Document
-    
-    private var formattedDocument: IMSDocument {
-        if let users = collection as? [UserModel] {
-            IMSDocument(text: FileFactory.makeUserStringFormatted(users: users))
-        } else if let histories = collection as? [InvoiceModel] {
-            IMSDocument(text: FileFactory.makeHistoryStringFormatted(histories: histories))
-        } else {
-            IMSDocument(text: "Nothing")
-        }
+}
+
+// MARK: - Formatted Document
+
+private func formattedDocument<T: Collection>(collection: T) -> IMSDocument {
+    if let users = collection as? [UserModel] {
+        IMSDocument(text: FileFactory.makeUserStringFormatted(users: users))
+    } else if let histories = collection as? [InvoiceModel] {
+        IMSDocument(text: FileFactory.makeHistoryStringFormatted(histories: histories))
+    } else if let products = collection as? [ProductModel] {
+        IMSDocument(text: FileFactory.makeProductStringFormatted(products: products))
+    } else {
+        IMSDocument(text: "Nothing")
     }
 }
